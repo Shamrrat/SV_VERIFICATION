1b57db44f4f2b6db2edc776683f17287d9e1103c
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2026-02-23 15:15:25 +0530

    Create 11.output.sv

---
+++ full_adder/11.output.sv
@@ -0,0 +1,32 @@
+----------------Generation of values-----------
+# a=1 b=1 c=0 sum=0 carry=0
+# ----------------Generation of values-----------
+# a=0 b=1 c=1 sum=0 carry=0
+# ----------------Generation of values-----------
+# a=1 b=1 c=0 sum=0 carry=0
+# ----------------received values of trans-----------
+# a=0 b=0 c=1 sum=0 carry=0
+# ----------------monitor received-----------
+# a=0 b=0 c=1 sum=1 carry=0
+# ----------------scoreboard received-----------
+# a=0 b=0 c=1 sum=1 carry=0
+# -----------------pass-------------
+# -------------transaction done--------
+# 
+# ----------------received values of trans-----------
+# a=0 b=0 c=1 sum=0 carry=0
+# ----------------monitor received-----------
+# a=0 b=0 c=1 sum=1 carry=0
+# ----------------scoreboard received-----------
+# a=0 b=0 c=1 sum=1 carry=0
+# -----------------pass-------------
+# -------------transaction done--------
+# 
+# ----------------received values of trans-----------
+# a=0 b=0 c=1 sum=0 carry=0
+# ----------------monitor received-----------
+# a=0 b=0 c=1 sum=1 carry=0
+# ----------------scoreboard received-----------
+# a=0 b=0 c=1 sum=1 carry=0
+# -----------------pass-------------
+# -------------transaction done--------
